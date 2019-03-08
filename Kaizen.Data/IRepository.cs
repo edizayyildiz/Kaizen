92c8d9e6f2082227371ac41c08a1793bb4f1d2e0
Ediz Ayyıldız <ayyildizediz@gmail.com>
2019-03-08 13:11:18 +0300

    Repository bitti.

---
+++ Kaizen.Data/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using Kaizen.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaizen.Data
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T Find(Guid id);
+        T Find(Expression<Func<T, bool>> where);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> where);
+
+
+
+    }
+}
