f97f4931b3efd0e8e3e6f697aaeb18b2e9baa159
Yigitcan Balcin <ybalcin1@gmail.com>
2019-03-08 13:23:58 +0300

    Data Katmanı tamamlandı.

---
+++ Kaizen.Data/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaizen.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext db;
+        private DbContextTransaction transaction;
+        public UnitOfWork(ApplicationDbContext db) 
+        {
+            this.db = db;
+        }
+        public void BeginTransaction()
+        {
+            transaction = db.Database.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            transaction.Commit();
+        }
+
+        public void Rollback()
+        {
+            transaction.Rollback();
+        }
+
+        public void SaveChanges()
+        {
+            db.SaveChanges();
+        }
+    }
+}
