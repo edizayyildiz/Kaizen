6681a8b3295872e351c17ec74bea9759195a98cb
edizayyildiz <ayyildizediz@gmail.com>
2019-03-10 19:53:51 +0300

    Suggestion(Create - Index)

---
+++ Kaizen.Web/Areas/Admin/Controllers/SuggestionController.cs
@@ -0,0 +1,44 @@
+﻿using Kaizen.Model;
+using Kaizen.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kaizen.Web.Areas.Admin.Controllers
+{
+    public class SuggestionController : Controller
+    {
+        private readonly ISuggestionService suggestionService;
+        public SuggestionController(ISuggestionService suggestionService)
+        {
+            this.suggestionService = suggestionService;
+        }
+        // GET: Admin/Suggestion
+        public ActionResult Index()
+        {
+            var suggestion = suggestionService.GetAll();
+            return View(suggestion);
+        }
+
+        public ActionResult Create()
+        {
+            var suggestion = new Suggestion();
+            suggestion.Assessment = Assessment.NoAssessment;
+            suggestion.Employee.FirstName = "Name";
+            return View(suggestion);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Suggestion suggestion)
+        {
+            if (ModelState.IsValid)
+            {
+                suggestionService.Insert(suggestion);
+                return RedirectToAction("index");
+            }
+            return View(suggestion);
+        }
+    }
+}
