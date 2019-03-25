6d2bae78794224eac83ea0f4580cb80cee558e63
Yigitcan Balcin <ybalcin1@gmail.com>
2019-03-25 14:23:52 +0300

    SeedFix

--- Kaizen.Data/ApplicationDbContextSeed.cs
+++ Kaizen.Data/ApplicationDbContextSeed.cs
@@ -16,9 +16,9 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("9854e4fd-6e1f-4c31-9f0d-65422dbd0bd9"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "Reason",
                 Sector = "Yazılım",
                 Description = "Reason firması açıklaması",
@@ -28,9 +28,9 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("a53a3120-f00a-4f3f-9a6b-5ebf3a1ede32"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "Microsoft",
                 Sector = "Yazılım",
                 Description = "Microsoft firması açıklaması",
@@ -42,18 +42,18 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("73756f28-9de2-4592-a7cc-4675d3bfedef"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "Türkiye"
             };
             var usa = new Country()
             {
                 Id = Guid.Parse("b780f09d-c2d1-4be0-8916-1ca1a6b5ecc6"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "USA"
             };
 
@@ -62,9 +62,9 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("6dbe0db1-763d-4ed1-8709-e7d7ce8a77a0"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "İstanbul",
                 CountryId = Guid.Parse("73756f28-9de2-4592-a7cc-4675d3bfedef")
             };
@@ -72,9 +72,9 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("8540d2c6-b95f-4129-8fe0-869db732045e"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "New York",
                 CountryId = Guid.Parse("b780f09d-c2d1-4be0-8916-1ca1a6b5ecc6")
             };
@@ -84,9 +84,9 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("48800942-4888-409e-89a0-e51a60a2e7ba"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "Beşiktaş",
                 CityId = Guid.Parse("6dbe0db1-763d-4ed1-8709-e7d7ce8a77a0")
             };
@@ -94,9 +94,9 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("6b04b845-0552-44ae-81b2-bbce7e3da273"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "Brooklyn",
                 CityId = Guid.Parse("8540d2c6-b95f-4129-8fe0-869db732045e")
             };
@@ -106,9 +106,9 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("b98c6a08-40ed-4a0f-a13a-9b7b772b2828"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "İstanbul Şubesi",
                 CompanyId = Guid.Parse("9854e4fd-6e1f-4c31-9f0d-65422dbd0bd9"),
                 CountryId = Guid.Parse("73756f28-9de2-4592-a7cc-4675d3bfedef"),
@@ -120,9 +120,9 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("9fcf4898-20c2-4e21-9948-62d954421955"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "New York Şubesi",
                 CompanyId = Guid.Parse("9854e4fd-6e1f-4c31-9f0d-65422dbd0bd9"),
                 CountryId = Guid.Parse("b780f09d-c2d1-4be0-8916-1ca1a6b5ecc6"),
@@ -134,9 +134,9 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("0e36930a-8756-4411-af32-5f087e0ecf27"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "Turkey Office",
                 CompanyId = Guid.Parse("a53a3120-f00a-4f3f-9a6b-5ebf3a1ede32"),
                 CountryId = Guid.Parse("73756f28-9de2-4592-a7cc-4675d3bfedef"),
@@ -166,9 +166,9 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("623a567d-e087-4040-befe-b1533aaa9794"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "Software",
                 BranchId = Guid.Parse("b98c6a08-40ed-4a0f-a13a-9b7b772b2828"),
             };
@@ -176,9 +176,9 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("7f3026e7-1e18-43af-ade0-ae3c64086697"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "İnsan Kaynakları",
                 BranchId = Guid.Parse("9fcf4898-20c2-4e21-9948-62d954421955"),
             };
@@ -186,9 +186,9 @@ namespace Kaizen.Data
             {
                 Id = Guid.Parse("5f31a2ae-76c2-445a-859d-912f1b813ab6"),
                 CreatedAt = DateTime.Now,
-                CreatedBy = "username",
+                CreatedBy = "admin@gmail.com",
                 UpdatedAt = DateTime.Now,
-                UpdatedBy = "username",
+                UpdatedBy = "admin@gmail.com",
                 Name = "Software",
                 BranchId = Guid.Parse("0e36930a-8756-4411-af32-5f087e0ecf27"),
             };
@@ -210,7 +210,7 @@ namespace Kaizen.Data
                 LockoutEndDateUtc = null,
                 AccessFailedCount = 0,
                 UserName = "admin@gmail.com",
-                Role = "SuperAdmin"
+                Role = "Developer"
             };
 
             context.Companies.AddOrUpdate(reason);
